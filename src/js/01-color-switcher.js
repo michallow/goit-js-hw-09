560738da29f9aaeadd3be45b8740a312895fc040
michallow <michallo_w@o2.pl>
2024-02-05 11:35:15 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let intervalId;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const changeBackgroundColor = color => {
+  document.body.style.backgroundColor = getRandomHexColor();
+};
+
+startBtn.addEventListener('click', () => {
+  intervalId = setInterval(changeBackgroundColor, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(intervalId);
+});
