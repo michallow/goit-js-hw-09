13d7339917335edff69f33b8d48b7b80d3fa333f
michallow <michallo_w@o2.pl>
2024-02-08 00:02:39 +0100

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,15 +2,12 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 const startBtn = document.querySelector('[data-start]');
-const stopBtn = document.querySelector('[data-stop]');
-const daysSpan = document.querySelector('[data-days]');
-const hoursSpan = document.querySelector('[data-hours]');
-const minutesSpan = document.querySelector('[data-minutes]');
-const secondsSpan = document.querySelector('[data-seconds]');
-
-const currentDate = new Date();
-
-let countdownInterval;
+let daysSpan = document.querySelector('[data-days]');
+let hoursSpan = document.querySelector('[data-hours]');
+let minutesSpan = document.querySelector('[data-minutes]');
+let secondsSpan = document.querySelector('[data-seconds]');
+let selectedDate = '';
+let currentDate = new Date();
 
 flatpickr('#datetime-picker', {
   enableTime: true,
@@ -18,20 +15,33 @@ flatpickr('#datetime-picker', {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose: selectedDates => {
-    const selectedDate = new Date(selectedDates[0]);
-    const currentDate = new Date();
+    console.log(selectedDates[0]);
+    selectedDate = new Date(selectedDates[0]);
+    // const currentDate = new Date();
     if (selectedDate < currentDate) {
-      console.log(startBtn);
+      window.alert('Please choose a date in the future');
       startBtn.setAttribute('disabled', true);
     } else {
-      console.log(startBtn);
       startBtn.removeAttribute('disabled');
     }
   },
 });
 
-// startBtn.addEventListener('submit', () => {
-//   console.log('alert');
-// });
+startBtn.addEventListener('click', () => {
+  setInterval(() => {
+    currentDate = new Date();
+    convertMs(selectedDate - currentDate);
+  }, 1000);
+});
 
-//   window.alert('Please choose a date in the future');
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  daysSpan.textContent = Math.floor(ms / day);
+  hoursSpan.textContent = Math.floor((ms % day) / hour);
+  minutesSpan.textContent = Math.floor(((ms % day) % hour) / minute);
+  secondsSpan.textContent = Math.floor((((ms % day) % hour) % minute) / second);
+}
